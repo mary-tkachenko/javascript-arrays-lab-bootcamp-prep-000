8be6a33ea9398e8d58ba460ba4d0768527b4a6fa
Mary <l.skyling@gmail.com>
2017-09-14 13:40:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -33,3 +33,10 @@ function prependKitten(name) {
   newKittens.unshift(name);
   return newKittens;
 }
+
+function removeLastKitten(name) {
+  var newKittens = [...kittens];
+  newKittens.pop(name);
+  return newKittens;
+}
+
