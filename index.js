62b7a21b1e5d809f5332f1088ccad634cbfb338f
Mary <l.skyling@gmail.com>
2017-09-14 13:30:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const app = "I don't do much."
+
 var kittens = ["Milo", "Otis", "Garfield"];
+
 function destructivelyAppendKitten(){
   kittens.push("Ralph");
+  return kittens;
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
   return kittens;
 }
